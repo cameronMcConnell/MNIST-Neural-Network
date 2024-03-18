2bd24d95f84082ecd77992a7c0004b1f2d1f7d50
Cameron McConnell <cameron@angry-dogman.net>
2024-03-18 16:19:43 -0700

    Added program to normalize the datasets.

---
+++ normalize.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import os
+
+TRAIN = os.path.join('./mnist_train.csv')
+TEST = os.path.join('./mnist_test.csv')
+
+train = pd.read_csv(TRAIN)
+test = pd.read_csv(TEST)
+
+labels_train = train.loc[:, 'label']
+labels_test = test.loc[:, 'label']
+
+norm_train = train.iloc[:, 1:].apply(lambda x: round(x / 255, 2))
+norm_test = test.iloc[:, 1:].apply(lambda x: round(x / 255, 2))
+
+train = pd.concat([labels_train, norm_train], axis=1)
+test = pd.concat([labels_test, norm_test], axis=1)
+
+train.to_csv(TRAIN)
+test.to_csv(TEST)
